cdc25b0545be42328a513d3abf53826b3ca751f3
Maria Granell Ruiz <mgranellruiz@gmail.com>
2022-11-22 19:18:49 +0100

    first animated plot

--- uk_museums_w47/uk_museums_w47.R
+++ uk_museums_w47/uk_museums_w47.R
@@ -44,6 +44,16 @@ data <- museums[,c("year_open", "year_closed", "Latitude", "Longitude", "Size")]
 data$Size <- as.numeric(ifelse(data$Size == "large", 3,
                                ifelse(data$Size == "small", 1,
                                       ifelse(data$Size == "medium", 2, "NA"))))
+# remove values with na
+na.omit(data)
+
+# make the years and int
+data$year_closed <- as.integer(data$year_closed)
+data$year_open <- as.integer(data$year_open)
+
+# see if the data is cooler from the 2000
+data <- data[data$year_open > 1999,]
+
 # colors
 col_dots <- "white"
 col_background <- "#11344F"
@@ -54,14 +64,15 @@ static <- ggplot() +
   geom_point(data = data, aes(x=Longitude, y= Latitude, size = Size, color= Size, alpha = Size)) +
   scale_size_continuous(name= "Museum size", breaks = c(1,2,3), labels = c("Small","Medium","Large")) +
   scale_alpha_continuous(name =  "Museum size", breaks = c(1,2,3), labels = c("Small","Medium","Large"), range = c(0.3,0.05)) +
-  #scale_colour_viridis(option= "magma", name = "Museum size", breaks =c(1,2,3)) +
   scale_color_gradientn(name = "Museum size", breaks =c(1,2,3), labels = c("Small","Medium","Large"), colours = col_dots) +
   ylim(50,59)+
   coord_map() + # make a map
   labs(
      title = "Uk museums over time",
-     subtitle = "Location of the different museums of the Uk, when they \noppened and which size they were",
+     subtitle = "Location of the different museums of the Uk, when they \noppened and which size they were
+     \n year",
      caption = "Source: · Graphic: Maria Granell Ruiz"
+     #  tag = "WHATAFUCK"
   ) +
   theme_void() +
   guides( colour = guide_legend()) + #to have one legend instead of two
@@ -72,11 +83,13 @@ static <- ggplot() +
         legend.background = element_rect(fill=col_background, color = col_background),
         plot.title = element_text(size = 20, face="bold"),
         plot.subtitle = element_text(size= 12)
+       # plot.tag = element_text()
   )
 
 #ggsave("test.png", bg = col_background, h = 5)
-
+static
 # animated plot --------------
-
-static + transition_reveal(year_open)+
-  labs(title="Year: {frame_time}")
+in_len <- as.integer(1)
+static + transition_events(start = year_open, end = year_closed, enter_length = in_len, exit_length = in_len) +
+  labs(subtitle = "Location of the different museums of the Uk, when they \noppened and which size they were
+     \n Year: {round(frame_time, 0)}")
